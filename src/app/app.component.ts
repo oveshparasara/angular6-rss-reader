718195bab01d2121e8bab99331ae6db57c37a7c8
ovesh <ovesh.parasara@gmail.com>
2018-09-12 21:41:43 +0530

    rss reader component in angular 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FeedServiceService} from "./feed-service.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private feedUrl: string = 'https%3A%2F%2Fwww.becompany.ch%2Fen%2Fblog%2Ffeed.xml';
+  private feeds: any;
+
+  constructor (
+    private feedService: FeedServiceService
+  ) {}
+
+  ngOnInit() {
+    this.refreshFeed();
+  }
+
+  private refreshFeed() {
+    this.feedService.getFeedContent(this.feedUrl)
+      .then(feed =>{
+        this.feeds = feed.items;
+      }).catch(error => {
+      console.log(error);
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FeedCardComponent } from './feed-card/feed-card.component';
+import {MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatToolbarModule} from "@angular/material";
+import {HttpModule} from "@angular/http";
+import {BrowserAnimationsModule, NoopAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas : [CUSTOM_ELEMENTS_SCHEMA , NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
